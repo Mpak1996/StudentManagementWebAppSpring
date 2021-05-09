0d8e4c5154a7a405f4d00df9f67a152109c7c278
Mpak1996 <giannismpak@outlook.com>
2021-05-09 13:32:36 +0300

    initialize SMA

---
+++ src/test/java/com/mpak/sma/StudentManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpak.sma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
